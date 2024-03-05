1997112fbc618c2162d603da3d2ac8f2dc3f22a5
Yulianna05k <162246556+Yulianna05k@users.noreply.github.com>
2024-03-04 19:27:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,17 @@ $nike = [
   "donde fue creado" => "Nueva York, y más en concreto -Harlem. ",
    "que tipo de tenis son" => "son zapatillas deportivas robustas y su suela presenta una plataforma elevada y curvada para brindar un buen soporte en el juego.",
 ];
+//Yulianna
+$puma = [
+  "marca" => "puma",
+  "modelo" => "Trinity"
+  "precio" => "3999",
+  "talla" => "26,26.5,27,28"
+  "color" => "verde,cafe,negro",
+  "año creado" => "1948"
+  "diseñador" => " Rudolf Dassler",
+  "envio" => "paqueteria"
+  "tienda" => "liverpool,amazon,coppel",
+  "material" => "cuerto autentico,poliester,goma"
 echo json_encode($nike);
+];
